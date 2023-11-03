eb69d4f949f1f5ad2308d231bd559fb4676c552c
helloworldhzx <657070781@qq.com>
2023-11-03 15:47:00 +0800

    Update index.js

--- sign/index.js
+++ sign/index.js
@@ -8,29 +8,31 @@ const SendInfo = require('./sendInfo')
 const { users } = require('./config')
 const { getCurPoint } = require('./help')
 exports.juejin = () => {
+  
   async function run() {
-    for (let index = 0; index < users.length; index++) {
-      const user = users[index]
-      const sender = new SendInfo(user)
-     try{
-//         await signIn(sender, user)
-//         console.log("签到--------");
-        vpnSignIn()
-        cySign()
-        await draw(sender, user)
-        console.log("抽奖--------");
-        await dipLucky(sender, user)
-        console.log("沾喜气--------");
-//         await playGame(sender, user)
-        const msg = await getCurPoint(user)
-        sender.addMsg(msg)
-        sender.sendAllMsg(user)
-     }catch(error){
-         console.log(error)
-         sender.addMsg("签到失败")
-         sender.sendAllMsg(user)
-     }
-    }
+    vpnSignIn()
+//     for (let index = 0; index < users.length; index++) {
+//       const user = users[index]
+//       const sender = new SendInfo(user)
+//      try{
+// //         await signIn(sender, user)
+// //         console.log("签到--------");
+//         vpnSignIn()
+//         //cySign()
+//         //await draw(sender, user)
+//        // console.log("抽奖--------");
+//         //await dipLucky(sender, user)
+//         //console.log("沾喜气--------");
+// //         await playGame(sender, user)
+//        // const msg = await getCurPoint(user)
+//         //sender.addMsg(msg)
+//         //sender.sendAllMsg(user)
+//      }catch(error){
+//          console.log(error)
+//          sender.addMsg("签到失败")
+//          sender.sendAllMsg(user)
+//      }
+//     }
   }
   run()
 }
